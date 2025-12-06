66b4f7700c9249d7964bcd18e6b01bc98d4faddb
PAMEH-2045 <1239g.g.g1239@gmail.com>
2025-12-06 12:47:48 +0300

    cleanup some code, fix mouse sitting-dragging glitch, fix LMB->ALT glitch, fix 'model wont go to dragging mode' glitch

--- AvatarRotationController.cs
+++ AvatarRotationController.cs
@@ -1,4 +1,5 @@
 ﻿using Kirurobo;
+using System.Reflection;
 using UnityEngine;
 
 [DefaultExecutionOrder(-10)]
@@ -9,14 +10,23 @@ public class AvatarRotationController : MonoBehaviour
     public float scrollRotationSpeed = 100f;
     public float mouseRotationSpeed = 5f;
 
-    private float targetRotation;
-    private bool isHolding;
-    private Vector3 lastMousePos;
-    private Transform modelRoot;
-    private GameObject currentModel;
-    private GameObject bubbleGO;
-    private MonoBehaviour avatarScaleController;
-    private AvatarAnimatorController controller;
+    float avatarScanInterval = 0.25f;
+    float nextAvatarScan;
+    bool suppressFrame;
+    float targetRotation;
+    bool isHolding;
+    Vector3 lastMousePos;
+    Transform modelRoot;
+    GameObject currentModel;
+    GameObject bubbleGO;
+    MonoBehaviour avatarScaleController;
+    AvatarAnimatorController controller;
+    FieldInfo controllerDragLockTimer;
+    FieldInfo controllerMouseHeld;
+    MethodInfo controllerSetDragging;
+    AvatarWindowHandler avatarWindowHandler;
+    FieldInfo targetCamera;
+    Camera targetCameraOrigin;
 
     void Start()
     {
@@ -31,49 +41,40 @@ public class AvatarRotationController : MonoBehaviour
     void Update()
     {
 
-        if (MenuActions.IsMovementBlocked())
-            return;
+        if (MenuActions.IsMovementBlocked()) return;
 
-        if (UniWindowController.current.isClickThrough && !isHolding)
-            return;
+        if (UniWindowController.current.isClickThrough && !isHolding) return;
 
-
-        if (modelRoot != null)
+        if (Time.unscaledTime >= nextAvatarScan)
         {
-            GameObject activeModel = null;
-            for (int i = 0; i < modelRoot.childCount; i++)
-            {
-                var child = modelRoot.GetChild(i);
-                if (child.gameObject.activeInHierarchy)
-                {
-                    activeModel = child.gameObject;
-                    break;
-                }
-            }
-
-            if (activeModel != currentModel)
-            {
-                currentModel = activeModel;
-                controller = currentModel != null ? currentModel.GetComponent<AvatarAnimatorController>() : null;
-                bubbleGO = currentModel.GetComponent<AvatarBubbleHandler>().attachTarget;
-            }
+            UpdateCurrentAvatar();
+            nextAvatarScan = Time.unscaledTime + avatarScanInterval;
         }
+
+        if (!currentModel) return;
+
+        if (suppressFrame) targetCamera.SetValue(avatarWindowHandler, targetCameraOrigin);
 
 
         bool leftBtn = Input.GetMouseButton(0);
         bool alt = Input.GetKey(KeyCode.LeftAlt);
 
-        if (isHolding)
-        {
-            isHolding = false;
-            controller.BlockDraggingOverride = false;
-        }
-        if (!alt || controller.isDragging)
-            return;
         if (leftBtn && alt)
         {
             isHolding = true;
             controller.BlockDraggingOverride = true;
+        }
+        else if (isHolding)
+        {
+            isHolding = false;
+            controllerDragLockTimer.SetValue(controller, 0f);
+            controllerMouseHeld.SetValue(controller, true);
+            controller.BlockDraggingOverride = false;
+
+            if (leftBtn)
+            {
+                controllerSetDragging.Invoke(controller, new object[] { true });
+            }
         }
 
 
@@ -90,9 +91,13 @@ public class AvatarRotationController : MonoBehaviour
             targetRotation = targetRotation + scroll * scrollRotationSpeed;
 
         }
-        else if (mouse != 0f && alt && leftBtn)
+        else if (alt && leftBtn)
         {
             avatarScaleController.enabled = false;
+
+            targetCamera.SetValue(avatarWindowHandler, null);
+            suppressFrame = true;
+
             targetRotation = targetRotation + mouse * mouseRotationSpeed;
         }
 
@@ -110,5 +115,36 @@ public class AvatarRotationController : MonoBehaviour
         }
 
         avatarScaleController.enabled = true;
+    }
+
+    void OnAvatarSwitch()
+    {
+        controller = currentModel.GetComponent<AvatarAnimatorController>();
+        controllerDragLockTimer = typeof(AvatarAnimatorController).GetField("dragLockTimer", BindingFlags.Instance | BindingFlags.NonPublic);
+        controllerMouseHeld = typeof(AvatarAnimatorController).GetField("mouseHeld", BindingFlags.Instance | BindingFlags.NonPublic);
+        controllerSetDragging = typeof(AvatarAnimatorController).GetMethod("SetDragging", BindingFlags.Instance | BindingFlags.NonPublic);
+
+        bubbleGO = currentModel.GetComponent<AvatarBubbleHandler>().attachTarget;
+
+        avatarWindowHandler = currentModel.GetComponent<AvatarWindowHandler>();
+        targetCamera = typeof(AvatarWindowHandler).GetField("targetCamera", BindingFlags.Instance | BindingFlags.Public);
+        targetCameraOrigin = (Camera)targetCamera.GetValue(avatarWindowHandler);
+    }
+
+    void UpdateCurrentAvatar()
+    {
+        if (!modelRoot) return;
+
+        for (int i = 0; i < modelRoot.childCount; i++)
+        {
+            var child = modelRoot.GetChild(i).gameObject;
+            if (!child.activeInHierarchy) continue;
+            if (currentModel == child) return;
+            currentModel = child;
+
+            OnAvatarSwitch();
+
+            return;
+        }
     }
 }
