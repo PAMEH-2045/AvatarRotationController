b52fcff8a45909f8b50cbd46d6350890748f395b
PAMEH-2045 <1239g.g.g1239@gmail.com>
2026-03-28 22:45:08 +0300

    some refactor

--- AvatarRotationController.cs
+++ AvatarRotationController.cs
@@ -8,12 +8,13 @@ public class AvatarRotationController : MonoBehaviour
     public float smoothFactor = 0.1f;
     public float scrollRotationSpeed = 100f;
     public float mouseRotationSpeed = 5f;
+    public float turnАroundThreshold = 65f;
 
     bool suppressFrame;
     float targetRotation;
     bool isHolding;
     Vector3 lastMousePos;
-    GameObject currentModel;
+    GameObject currentModelGO;
     GameObject bubbleGO;
     MonoBehaviour avatarScaleController;
     AvatarAnimatorController controller;
@@ -39,83 +40,44 @@ public class AvatarRotationController : MonoBehaviour
     void Update()
     {
         if (MenuActions.IsMovementBlocked()) return;
-
         if (UniWindowController.current.isClickThrough && !isHolding) return;
-
-        if (!currentModel) return;
+        if (currentModelGO == null) return;
 
         if (suppressFrame) avatarWindowHandler.targetCamera = targetCameraOrigin;
 
-        var modelRotation = CurrentModel.GameObject.transform.rotation.eulerAngles.y;
-        if (modelRotation <= 65f || modelRotation >= 295f)
-        {
-            trackingCam.transform.position = mirroredMainCamPos;
-        }
-        else
-        {
-            trackingCam.transform.position = Camera.main.transform.position;
-        }
+        UpdateTrackingCamera();
 
-        bool leftBtn = Input.GetMouseButton(0);
         bool alt = Input.GetKey(KeyCode.LeftAlt);
+        bool leftBtn = Input.GetMouseButton(0);
+        bool mouseRotation = alt && leftBtn;
+        bool scrollRotation = alt && Input.mouseScrollDelta.y != 0f;
 
-        if (leftBtn && alt)
+        UpdateHoldingState(alt, leftBtn);
+
+        targetRotation = currentModelGO.transform.localRotation.eulerAngles.y;
+
+        if (mouseRotation || scrollRotation)
         {
-            isHolding = true;
-            controller.BlockDraggingOverride = true;
-        }
-        else if (isHolding)
-        {
-            isHolding = false;
-            var isBigScreenActive = CurrentModel.C<AvatarBigScreenHandler>.Get<bool>("isBigScreenActive");
-            if (!isBigScreenActive)
+            avatarScaleController.enabled = false;
+            if (mouseRotation)
             {
-                CurrentModel.C<AvatarAnimatorController>.Set("dragLockTimer", 0f);
-                CurrentModel.C<AvatarAnimatorController>.Set("mouseHeld", true);
-                controller.BlockDraggingOverride = false;
-                if (leftBtn)
-                {
-                    CurrentModel.C<AvatarAnimatorController>.Call("SetDragging", true);
-                }
+                var currentMousePos = Input.mousePosition;
+                var mouseDeltaX = (lastMousePos - currentMousePos).x;
+                lastMousePos = currentMousePos;
+
+                targetRotation += mouseDeltaX * mouseRotationSpeed;
+
+                avatarWindowHandler.targetCamera = null;
+                suppressFrame = true;
+            }
+            else if (scrollRotation)
+            {
+                float scrollDelta = Input.mouseScrollDelta.y;
+                targetRotation += scrollDelta * scrollRotationSpeed;
             }
         }
 
-
-        var currentMousePos = Input.mousePosition;
-        var mouse = (lastMousePos - currentMousePos).x;
-        lastMousePos = currentMousePos;
-
-        float scroll = Input.mouseScrollDelta.y;
-
-        targetRotation = currentModel.transform.localRotation.eulerAngles.y;
-        if (scroll != 0f && alt)
-        {
-            avatarScaleController.enabled = false;
-            targetRotation = targetRotation + scroll * scrollRotationSpeed;
-
-        }
-        else if (alt && leftBtn)
-        {
-            avatarScaleController.enabled = false;
-
-            avatarWindowHandler.targetCamera = null;
-            suppressFrame = true;
-
-            targetRotation = targetRotation + mouse * mouseRotationSpeed;
-        }
-
-        float current = currentModel.transform.localRotation.eulerAngles.y;
-        float smoothed = Mathf.Lerp(
-            current,
-            targetRotation,
-            1f - Mathf.Pow(1f - smoothFactor, Time.deltaTime * 60f)
-        );
-
-        if (Mathf.Abs(smoothed - current) > 0.0001f)
-        {
-            currentModel.transform.localEulerAngles = new Vector3(0, smoothed, 0);
-            bubbleGO.transform.localEulerAngles = new Vector3(0, smoothed, 0);
-        }
+        ApplyRotation();
 
         avatarScaleController.enabled = true;
     }
@@ -125,7 +87,7 @@ public class AvatarRotationController : MonoBehaviour
     }
     void OnAvatarSwitch()
     {
-        currentModel = CurrentModel.GameObject;
+        currentModelGO = CurrentModel.GameObject;
 
         controller = CurrentModel.GetComponent<AvatarAnimatorController>();
         avatarScaleController = CurrentModel.GetComponent<AvatarScaleController>();
@@ -135,5 +97,60 @@ public class AvatarRotationController : MonoBehaviour
         targetCameraOrigin = avatarWindowHandler.targetCamera;
 
         CurrentModel.C<AvatarMouseTracking>.F["mainCam"] = trackingCam;
+    }
+    void UpdateTrackingCamera()
+    {
+        var modelRotation = CurrentModel.GameObject.transform.rotation.eulerAngles.y;
+        if (Mathf.Abs(Mathf.DeltaAngle(0f, modelRotation)) <= turnАroundThreshold)
+        {
+            trackingCam.transform.position = mirroredMainCamPos;
+        }
+        else
+        {
+            trackingCam.transform.position = Camera.main.transform.position;
+        }
+    }
+    void UpdateHoldingState(bool alt, bool leftBtn)
+    {
+        if (alt & leftBtn)
+        {
+            if (!isHolding)
+            {
+                isHolding = true;
+                controller.BlockDraggingOverride = true;
+            }
+        }
+        else if (isHolding)
+        {
+            isHolding = false;
+
+            bool isBigScreenActive = CurrentModel.C<AvatarBigScreenHandler>.Get<bool>("isBigScreenActive");
+            if (!isBigScreenActive)
+            {
+                CurrentModel.C<AvatarAnimatorController>.Set("dragLockTimer", 0f);
+                CurrentModel.C<AvatarAnimatorController>.Set("mouseHeld", true);
+                controller.BlockDraggingOverride = false;
+
+                if (leftBtn)
+                {
+                    CurrentModel.C<AvatarAnimatorController>.Call("SetDragging", true);
+                }
+            }
+        }
+    }
+    void ApplyRotation()
+    {
+        float currentRotation = currentModelGO.transform.localRotation.eulerAngles.y;
+
+        float t = 1f - Mathf.Pow(1f - smoothFactor, Time.deltaTime * 60f);
+        //float t = 1f - Mathf.Exp(-smoothFactor * Time.deltaTime);
+        float smoothedRotation = Mathf.Lerp(currentRotation, targetRotation, t);
+
+        if (Mathf.Abs(smoothedRotation - currentRotation) > 0.0001f)
+        {
+            Vector3 newRotation = new Vector3(0f, smoothedRotation, 0f);
+            currentModelGO.transform.localEulerAngles = newRotation;
+            bubbleGO.transform.localEulerAngles = newRotation;
+        }
     }
 }
