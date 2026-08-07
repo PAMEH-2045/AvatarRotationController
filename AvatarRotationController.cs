7fc294d9687c7313be82ee9478488afaabc66728
PAMEH-2045 <1239g.g.g1239@gmail.com>
2026-08-07 08:40:11 +0300

    fix spine rotation when turned around
    + change namespace
    + fix to-dance transition attempt while holding and target app is playing music

--- AvatarRotationController.cs
+++ AvatarRotationController.cs
@@ -1,155 +1,184 @@
 ﻿using Kirurobo;
+using NAudio.CoreAudioApi;
 using UnityEngine;
 
-[DefaultExecutionOrder(-10)] // Should run before AvatarScaleController to disable it properly
-public class AvatarRotationController : MonoBehaviour
+namespace AvatarRotationController
 {
-    [SerializeField] private float scrollRotationSpeed = 10f;
-    [SerializeField] private float mouseRotationSpeed = 0.5f;
-    [SerializeField] private float turnАroundThreshold = 65f;
-    [SerializeField] private bool turnАround = true;
-    [SerializeField] private bool blockDragging = true;
-
-    bool isHolding;
-    Vector3 lastMousePos;
-
-    GameObject bubbleGO;
-
-    MonoBehaviour avatarScaleController;
-    AvatarAnimatorController controller;
-
-    AvatarWindowHandler avatarWindowHandler;
-    Camera targetCameraOrigin;
-
-    Vector3 mirroredMainCamPos;
-    Camera trackingCam;
-
-    void OnEnable()
+    [DefaultExecutionOrder(-10)] // Should run before AvatarScaleController to disable it properly
+    public class AvatarRotationController : MonoBehaviour
     {
-        CurrentModel.OnAvatarSwitch += OnAvatarSwitch;
-    }
-    void Start()
-    {
-        CurrentModel.OnStart();
+        [SerializeField] private float scrollRotationSpeed = 10f;
+        [SerializeField] private float mouseRotationSpeed = 0.5f;
+        [SerializeField] private float turnАroundThreshold = 65f;
+        [SerializeField] private bool turnАround = true;
+        [SerializeField] private bool blockDragging = true;
 
-        trackingCam = transform.GetComponentInChildren<Camera>(true);
-        trackingCam.nearClipPlane = Camera.main.nearClipPlane;
-        mirroredMainCamPos = new Vector3(
-            Camera.main.transform.position.x, 
-            Camera.main.transform.position.y,
-            CurrentModel.Root.position.z - Camera.main.transform.position.z
-        );
+        bool isHolding;
+        Vector3 lastMousePos;
 
-        avatarScaleController = GameObject.FindFirstObjectByType<AvatarScaleController>();
+        GameObject bubbleGO;
 
-        try
+        MonoBehaviour avatarScaleController;
+        AvatarAnimatorController controller;
+
+        AvatarWindowHandler avatarWindowHandler;
+        Camera targetCameraOrigin;
+
+        Vector3 mirroredMainCamPos;
+        Camera trackingCam;
+
+        MMDevice defaultDeviceOrigin;
+
+        public static bool isTurnedAround;
+
+        void OnEnable()
         {
-            Settings.AddSettings(this);
-        } 
-        catch { }
-    }
-    void Update()
-    {
-        CurrentModel.OnUpdate();
-
-        if (CurrentModel.gameObject == null) return;
-        if (UniWindowController.current.isClickThrough && !isHolding) return;
-
-        avatarWindowHandler.targetCamera = targetCameraOrigin;
-
-        UpdateTrackingCamera();
-
-        bool alt = Input.GetKey(KeyCode.LeftAlt);
-        bool leftBtn = Input.GetMouseButton(0);
-        bool mouseRotation = alt && leftBtn;
-        bool scrollRotation = alt && Input.mouseScrollDelta.y != 0f;
-
-        UpdateHoldingState(alt, leftBtn);
-
-        var currentMousePos = Input.mousePosition;
-        var mouseDeltaX = (lastMousePos - currentMousePos).x;
-        lastMousePos = currentMousePos;
-
-        if (!mouseRotation && !scrollRotation) return;
-
-        avatarScaleController.enabled = false;
-
-        var targetRotation = CurrentModel.transform.localRotation.eulerAngles.y;
-
-        if (mouseRotation)
-        {
-            targetRotation += mouseDeltaX * mouseRotationSpeed;
-
-            avatarWindowHandler.targetCamera = null;
+            CurrentModel.OnAvatarSwitch += OnAvatarSwitch;
         }
-        else if (scrollRotation)
+        void Start()
         {
-            float scrollDelta = Input.mouseScrollDelta.y;
-            targetRotation += scrollDelta * scrollRotationSpeed;
-        }
-        
-        ApplyRotation(targetRotation);
+            CurrentModel.OnStart();
 
-        avatarScaleController.enabled = true;
-    }
-    void OnDisable()
-    {
-        CurrentModel.OnAvatarSwitch -= OnAvatarSwitch;
-    }
-    void OnAvatarSwitch()
-    {
-        controller = CurrentModel.AvatarAnimatorControllerProxy.Inst;
+            trackingCam = transform.GetComponentInChildren<Camera>(true);
+            trackingCam.nearClipPlane = Camera.main.nearClipPlane;
+            mirroredMainCamPos = new Vector3(
+                Camera.main.transform.position.x,
+                Camera.main.transform.position.y,
+                CurrentModel.Root.position.z - Camera.main.transform.position.z
+            );
 
-        bubbleGO = CurrentModel.AvatarBubbleHandlerProxy.Inst.attachTarget;
-        avatarWindowHandler = CurrentModel.AvatarWindowHandlerProxy.Inst;
-        targetCameraOrigin = avatarWindowHandler.targetCamera;
+            avatarScaleController = GameObject.FindFirstObjectByType<AvatarScaleController>();
 
-        CurrentModel.AvatarMouseTrackingProxy.mainCam = trackingCam;
-    }
-    void UpdateTrackingCamera()
-    {
-        var modelRotation = CurrentModel.transform.rotation.eulerAngles.y;
-        if (Mathf.Abs(Mathf.DeltaAngle(0f, modelRotation)) <= turnАroundThreshold && turnАround)
-        {
-            trackingCam.transform.position = mirroredMainCamPos;
-        }
-        else
-        {
-            trackingCam.transform.position = Camera.main.transform.position;
-        }
-    }
-    void UpdateHoldingState(bool alt, bool leftBtn)
-    {
-        if (alt & leftBtn)
-        {
-            if (!isHolding)
+            try
             {
-                isHolding = true;
-                if (blockDragging) controller.BlockDraggingOverride = true;
+                Settings.AddSettings(this);
+            }
+            catch { }
+        }
+        void Update()
+        {
+            CurrentModel.OnUpdate();
+
+            if (CurrentModel.gameObject == null) return;
+            if (UniWindowController.current.isClickThrough && !isHolding) return;
+
+            avatarWindowHandler.targetCamera = targetCameraOrigin;
+
+            UpdateTrackingCamera();
+
+            bool alt = Input.GetKey(KeyCode.LeftAlt);
+            bool leftBtn = Input.GetMouseButton(0);
+            bool mouseRotation = alt && leftBtn;
+            bool scrollRotation = alt && Input.mouseScrollDelta.y != 0f;
+
+            UpdateHoldingState(alt, leftBtn);
+
+            var currentMousePos = Input.mousePosition;
+            var mouseDeltaX = (lastMousePos - currentMousePos).x;
+            lastMousePos = currentMousePos;
+
+            if (!mouseRotation && !scrollRotation) return;
+
+            avatarScaleController.enabled = false;
+
+            var targetRotation = CurrentModel.transform.localRotation.eulerAngles.y;
+
+            if (mouseRotation)
+            {
+                targetRotation += mouseDeltaX * mouseRotationSpeed;
+
+                avatarWindowHandler.targetCamera = null;
+            }
+            else if (scrollRotation)
+            {
+                float scrollDelta = Input.mouseScrollDelta.y;
+                targetRotation += scrollDelta * scrollRotationSpeed;
+            }
+
+            ApplyRotation(targetRotation);
+
+            avatarScaleController.enabled = true;
+        }
+        void LateUpdate()
+        {
+            SpineRotator.OnPreLateUpdate();
+        }
+        void OnDisable()
+        {
+            CurrentModel.OnAvatarSwitch -= OnAvatarSwitch;
+        }
+        void OnAvatarSwitch()
+        {
+            controller = CurrentModel.AvatarAnimatorController.Inst;
+
+            bubbleGO = CurrentModel.AvatarBubbleHandler.Inst.attachTarget;
+            avatarWindowHandler = CurrentModel.AvatarWindowHandler.Inst;
+            targetCameraOrigin = avatarWindowHandler.targetCamera;
+
+            CurrentModel.AvatarMouseTracking.mainCam = trackingCam;
+
+            defaultDeviceOrigin = CurrentModel.AvatarAnimatorController.defaultDevice;
+        }
+        void UpdateTrackingCamera()
+        {
+            if (!SaveLoadHandler.Instance.data.enableMouseTracking)
+            {
+                if (isTurnedAround)
+                {
+                    trackingCam.transform.position = Camera.main.transform.position;
+                    isTurnedAround = false;
+                }  
+                
+                return;
+            }
+
+            var modelRotation = CurrentModel.transform.rotation.eulerAngles.y;
+            if (Mathf.Abs(Mathf.DeltaAngle(0f, modelRotation)) <= turnАroundThreshold && turnАround)
+            {
+                trackingCam.transform.position = mirroredMainCamPos;
+                isTurnedAround = true;
+            }
+            else
+            {
+                trackingCam.transform.position = Camera.main.transform.position;
+                isTurnedAround = false;
             }
         }
-        else if (isHolding)
+        void UpdateHoldingState(bool alt, bool leftBtn)
         {
-            isHolding = false;
-
-            bool isBigScreenActive = CurrentModel.AvatarBigScreenHandlerProxy.isBigScreenActive;
-            if (!isBigScreenActive && blockDragging)
+            if (alt & leftBtn)
             {
-                CurrentModel.AvatarAnimatorControllerProxy.dragLockTimer = 0f;
-                CurrentModel.AvatarAnimatorControllerProxy.mouseHeld = true;
-                controller.BlockDraggingOverride = false;
-
-                if (leftBtn)
+                if (!isHolding)
                 {
-                    CurrentModel.AvatarAnimatorControllerProxy.SetDragging(true);
+                    isHolding = true;
+                    CurrentModel.AvatarAnimatorController.defaultDevice = null;
+                    if (blockDragging) controller.BlockDraggingOverride = true;
+                }
+            }
+            else if (isHolding)
+            {
+                isHolding = false;
+
+                CurrentModel.AvatarAnimatorController.defaultDevice = defaultDeviceOrigin;
+
+                if (!CurrentModel.IsBigScreenActive && blockDragging)
+                {
+                    CurrentModel.AvatarAnimatorController.dragLockTimer = 0f;
+                    CurrentModel.AvatarAnimatorController.mouseHeld = true;
+                    controller.BlockDraggingOverride = false;
+
+                    if (leftBtn)
+                    {
+                        CurrentModel.AvatarAnimatorController.SetDragging(true);
+                    }
                 }
             }
         }
-    }
-    void ApplyRotation(float targetRotation)
-    {
-        Vector3 newRotation = new Vector3(0f, targetRotation, 0f);
-        CurrentModel.transform.localEulerAngles = newRotation;
-        bubbleGO.transform.localEulerAngles = newRotation;
+        void ApplyRotation(float targetRotation)
+        {
+            Vector3 newRotation = new Vector3(0f, targetRotation, 0f);
+            CurrentModel.transform.localEulerAngles = newRotation;
+            bubbleGO.transform.localEulerAngles = newRotation;
+        }
     }
 }
---
+++ SpineRotator.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+namespace AvatarRotationController
+{
+    [DefaultExecutionOrder(+10)] // Should run after AvatarMouseTracking to override its values
+    internal class SpineRotator : MonoBehaviour
+    {
+        static Transform spineBone => CurrentModel.AvatarMouseTracking.spineBone;
+        static Transform chestBone => CurrentModel.AvatarMouseTracking.chestBone;
+        static Transform spineDriver => CurrentModel.AvatarMouseTracking.spineDriver;
+        static Transform upperChestBone => CurrentModel.AvatarMouseTracking.upperChestBone;
+        static Quaternion spineInitRot => CurrentModel.AvatarMouseTracking.spineInitRot;
+        static float spineBlend => CurrentModel.AvatarMouseTracking.Inst.spineBlend;
+        static float spineSmoothness => CurrentModel.AvatarMouseTracking.Inst.spineSmoothness;
+        static float spineMinRotation => CurrentModel.AvatarMouseTracking.Inst.spineMinRotation;
+        static float spineMaxRotation => CurrentModel.AvatarMouseTracking.Inst.spineMaxRotation;
+
+        static Quaternion cachedDriverRotation;
+        static Quaternion cachedSpineRotation;
+        static Quaternion cachedChestRotation;
+        static Quaternion cachedUpperChestRotation;
+        internal static void OnPreLateUpdate() // LateUpdate before AvatarMouseTracking
+        {
+            if (AvatarRotationController.isTurnedAround)
+            {
+                cachedDriverRotation = spineDriver.localRotation;
+                cachedSpineRotation = spineBone.localRotation;
+                cachedChestRotation = chestBone.localRotation;
+                cachedUpperChestRotation = upperChestBone.localRotation;
+            }
+        }
+        void LateUpdate() // LateUpdate after AvatarMouseTracking
+        {
+            if (AvatarRotationController.isTurnedAround)
+            {
+                DoReversedSpine();
+            }
+        }
+        void DoReversedSpine()
+        {
+            if (!spineBone || !spineDriver) return;
+
+            //float targetW = IsAllowed("Spine") ? 1f : 0f;
+            //spineTrackingWeight = Mathf.MoveTowards(spineTrackingWeight, targetW, Time.deltaTime * spineFadeSpeed);
+
+            float normY = Mathf.Clamp01(Input.mousePosition.x / Screen.width);
+            float targetY = Mathf.Lerp(spineMinRotation, spineMaxRotation, normY);
+
+            spineDriver.localRotation = Quaternion.Slerp(cachedDriverRotation, Quaternion.Euler(0f, targetY, 0f), Time.deltaTime * spineSmoothness);
+            //spineDriver.localRotation = Quaternion.Slerp(spineDriver.localRotation, Quaternion.Euler(0f, -targetY, 0f), Time.deltaTime * spineSmoothness);
+
+            var baseRot = cachedSpineRotation;
+            var delta = spineDriver.localRotation * Quaternion.Inverse(spineInitRot);
+
+            float applied = CurrentModel.AvatarMouseTracking.spineTrackingWeight * spineBlend;
+            var offset = Quaternion.Slerp(Quaternion.identity, delta, applied);
+
+            spineBone.localRotation = offset * baseRot;
+            if (chestBone)
+                chestBone.localRotation = Quaternion.Slerp(Quaternion.identity, delta, 0.8f * applied) * cachedChestRotation;
+            if (upperChestBone)
+                upperChestBone.localRotation = Quaternion.Slerp(Quaternion.identity, delta, 0.6f * applied) * cachedUpperChestRotation;
+        }
+    }
+}
